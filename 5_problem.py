b72b913f23a9b525c56605d6ef4772f633c89fb1
“Arpita <arpitakumari10653@gmail.com>
2025-06-17 16:29:17 +0530

    a log file and find out whether it contains ‘python’

--- 5_problem.py
+++ 5_problem.py
@@ -3,8 +3,8 @@ words = ["Donkey" , "fool"]
 with open("file.txt", "r") as f:
     content = f.read()
 for word in words:
-    contentNew = content.replace(word, "#" * len(word))
+    content = content.replace(word, "#" * len(word))
 
 with open("file.txt", "w") as f:
-    f.write(contentNew)
+    f.write(content)
     
