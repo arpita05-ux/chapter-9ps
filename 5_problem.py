9165e0eaf33a9de38a5a2716fcd2756520c92b29
“Arpita <arpitakumari10653@gmail.com>
2025-06-17 12:12:25 +0530

    list of such words to be censored

---
+++ 5_problem.py
@@ -0,0 +1,10 @@
+words = ["Donkey" , "fool"]
+
+with open("file.txt", "r") as f:
+    content = f.read()
+for word in words:
+    contentNew = content.replace(word, "#" * len(word))
+
+with open("file.txt", "w") as f:
+    f.write(contentNew)
+    
